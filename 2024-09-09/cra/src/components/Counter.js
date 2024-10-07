2ae3309d7d4253378ea301d2589ad1f58eb8e39a
Morten-Paul Mühlberg <morten1@tlu.ee>
2024-10-07 14:22:38 +0300

    feat: counter homework

--- 2024-09-09/cra/src/components/Counter.js
+++ 2024-09-09/cra/src/components/Counter.js
@@ -2,19 +2,20 @@ import React, { useState } from "react"
 
 const Counter = () => {
   const [counter, setCounter] = useState(0)
-  // const [counter, setCounter] = React.useState(0);
 
-  const modifyCounter = () => setCounter(prevCounter => prevCounter + 1)
+  const modifyCounter = (value) => setCounter((prevCounter) => prevCounter + value);
 
   return (
     <>
       <h1>{counter}</h1>
 
-      {[+1, +5, +50, -1, -5, -50].map(element => (
-        <button onClick={modifyCounter}>sync {element}</button>
+      {[+1, +5, +50, -1, -5, -50].map((element) => (
+        <button key={element} onClick={() => modifyCounter(element)}>
+          {element > 0 ? `+${element}` : element}
+        </button>
       ))}
 
-      <button onClick={() => setTimeout(() => modifyCounter(), 2000)}>
+      <button onClick={() => setTimeout(() => modifyCounter(1), 2000)}>
         async +1
       </button>
     </>
