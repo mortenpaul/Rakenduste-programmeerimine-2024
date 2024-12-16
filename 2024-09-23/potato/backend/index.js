df872185a30363a719d5604be39b564251f39bea
Morten-Paul Mühlberg <70900314+mortenpaul@users.noreply.github.com>
2024-12-16 15:05:01 +0200

    feat: tunnitöö järgi tegemine

--- 2024-09-23/potato/backend/index.js
+++ 2024-09-23/potato/backend/index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/0x0', (req, res) => {
+  res.send('0x0ss')
+})
+
+app.get('/users/:userId/books/:bookId', (req, res) => {
+  res.send(req.params)
+})
+
+app.get('/posts/:postID', (req, res) => {
+  res.send(req.params)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
