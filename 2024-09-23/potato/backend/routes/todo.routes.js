2180d380b10c7ab7d618ac8e91b3daae81dd96e7
Morten-Paul Mühlberg <70900314+mortenpaul@users.noreply.github.com>
2024-12-21 03:10:29 +0200

    fix: fix JWT

--- 2024-09-23/potato/backend/routes/todo.routes.js
+++ 2024-09-23/potato/backend/routes/todo.routes.js
@@ -7,4 +7,8 @@ router.post("/", todoController.create);
 router.put("/", todoController.update);
 router.delete("/", todoController.delete);
 
+//JWT
+router.get("/token", todoController.getToken);
+router.post("/verify", todoController.verifyToken);
+
 module.exports = router;
